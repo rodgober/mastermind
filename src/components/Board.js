6f025f4d6a442e921846d633c21da86c833af9d9
Rodrigo Bernardino <rodgober7@hotmail.com>
2021-08-23 16:59:19 -0500

    Proyecto inicial

---
+++ src/components/Board.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Board = () => {
+    return ( <h1>
+        Board
+    </h1> );
+}
+ 
+export default Board;<h1>
+Board</h1>
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const Footer = () => {
+    return ( 
+        <h1>Copyright</h1>
+     );
+}
+ 
+export default Footer;
